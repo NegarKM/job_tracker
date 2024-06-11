179d404c55796ef64bf1666538baac72e1e9c0e6
Negar Kermanshahimonfared <negar@Negars-MacBook-Pro.local>
2024-06-11 12:51:06 -0400

    first commit

---
+++ src/test/java/com/thirty3/job/job_tracker/JobTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thirty3.job.job_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
