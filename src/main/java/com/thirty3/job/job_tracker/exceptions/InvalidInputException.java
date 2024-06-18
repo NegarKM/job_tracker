a129071a22eaff0afd9788777ecd181957866453
Negar Kermanshahimonfared <negar@Negars-MacBook-Pro.local>
2024-06-18 17:01:57 -0400

    add endpoints: delete job-description, update job-description, update job-description status - add tests

---
+++ src/main/java/com/thirty3/job/job_tracker/exceptions/InvalidInputException.java
@@ -0,0 +1,7 @@
+package com.thirty3.job.job_tracker.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class InvalidInputException extends RuntimeException {}
